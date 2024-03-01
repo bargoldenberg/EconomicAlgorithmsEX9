72229890bd76437a72d48a870e42b8db9bc7f342
Bar Goldenberg <bar.goldenberg@similarweb.com>
2024-03-01 16:57:24 +0200

    added tests

--- find_decomposition.py
+++ find_decomposition.py
@@ -13,7 +13,9 @@ def get_players_for_topic(preferences):
 
 def find_decomposition(budget: list[float], preferences: list[set[int]]):
   players_per_topic = get_players_for_topic(preferences)
+  print(players_per_topic) 
   player_budget = sum(budget) / len(preferences)
+  print(player_budget)
   players_money = [player_budget for _ in range(len(preferences))]
   ans = []
   for i in range(len(budget)):
@@ -22,6 +24,10 @@ def find_decomposition(budget: list[float], preferences: list[set[int]]):
       return []
     for player in players_per_topic[i]:
         if players_money[player] <= 0:
+          continue
+        if budget[i] == players_money[player]:
+          players_money[player] = 0
+          ans.append(f"player {player} gave {budget[i]} to item {i}")
           continue
         amount_to_give = player_budget if budget[i] == len(players_per_topic[i])*player_budget else (player_budget - budget[i])
         players_money[player] -= amount_to_give
@@ -54,5 +60,14 @@ ans = find_decomposition(budget, preferences)
 # player 0 gave 100.0 to item 0
 # player 1 gave 100.0 to item 1
 # player 2 gave 100.0 to item 1
+for purchase in ans:
+  print(purchase)
+
+budget = [150, 150]
+preferences = [{0, 1}, {0, 1}]
+ans = find_decomposition(budget, preferences)
+#Expected - 
+# player 0 gave 150 to item 0
+# player 1 gave 150 to item 0
 for purchase in ans:
   print(purchase)
